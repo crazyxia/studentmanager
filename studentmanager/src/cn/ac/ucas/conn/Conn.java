394231d09576fd7b170071bdebff245f7480c6dc
xiachaochao <2252916592@qq.com>
2017-06-12 19:17:55 +0800

    网上找到的代码

---
+++ studentmanager/src/cn/ac/ucas/conn/Conn.java
@@ -0,0 +1,28 @@
+package cn.ac.ucas.conn;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conn {
+	
+	public Connection getConn()  {
+		Connection conn = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url="jdbc:mysql://127.0.0.1/student?useUicode=true&characterEncoding=utf-8";
+			String user="root";
+			String password="123456";
+            conn=DriverManager.getConnection(url, user, password);			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		return conn;
+	}
+
+	
+
+}
